08df2df144a0af78f1fc17cca7c0550412072cc4
Ivan Martynov <martynovivan611@gmail.com>
2026-09-07 15:27:36 +0300

    Lab01 Task02: calculate visit cost with discount

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Task1;
 Console.WriteLine("Hello, World!");
 
-Task1.Task1.Run();
+Task2.Run();
---
+++ Lab1/Task2.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Task1;
+
+public class Task2
+{
+    public static void Run()
+    {
+        double price = 0, result;
+        int count = 0, discount = 0;
+        
+        Console.Write("Enter the price of the product: ");
+        price = double.Parse(Console.ReadLine());
+        
+        Console.Write("Enter the number of products: ");
+        count = int.Parse(Console.ReadLine());
+        
+        Console.Write("Enter the discount: ");
+        discount = int.Parse(Console.ReadLine());
+        
+        result = price * count * (1 - discount / 100.0);
+        Console.Write($"Result: {result:F2}");
+    }
+}
