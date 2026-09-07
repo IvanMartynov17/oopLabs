c89523a93ed2cb68edc85626bb1f4ea2d0b65faf
Ivan Martynov <martynovivan611@gmail.com>
2026-09-07 15:21:01 +0300

    Lab01 Task01: calculate BMI from weight and height

---
+++ Lab1/Program.cs
@@ -0,0 +1,4 @@
+﻿using Task1;
+Console.WriteLine("Hello, World!");
+
+Task1.Task1.Run();
---
+++ Lab1/Task1.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1;
+
+public class Task1
+{
+    public static void Run()
+    {
+        double weight;
+        double height;
+
+        Console.WriteLine("Enter your weight in kilograms: ");
+        weight = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter your height in cm: ");
+        height = double.Parse(Console.ReadLine());
+
+        double bmi = weight / (height * height) * 10000;
+        Console.Write($"BMI: {bmi:F2}");
+    }
+}
