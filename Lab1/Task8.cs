f266be5b0b45d4f2d489449c2d45d8d7497657f3
Ivan Martynov <martynovivan611@gmail.com>
2026-09-08 22:02:39 +0300

    Corrected Task8

--- Lab1/Task8.cs
+++ Lab1/Task8.cs
@@ -1,69 +1,67 @@
 ﻿namespace Task1;
 
-public class Task8
+public static class Task8
 {
-    public static double CalculateBMI(double weight, double height)
+    static double CalculateBMI(double weight, double height)
     {
         return weight / (height * height) * 10000;
     }
 
-    public static string GetBMICategory(double bmi)
+    static string GetBMICategory(double bmi)
     {
         if (bmi > 18.5 && bmi < 24.9)
         {
-            return "Norma";
+            return "Норма";
         }
         else if (bmi > 25 && bmi < 29.9)
         {
-            return "Excess weight";
+            return "Надмірня вага";
         }
-        else if (bmi > 30)
+        else if (bmi >= 30)
         {
-            return "Overweight";
+            return "Ожиріння";
         }else
         {
-            return "Underweight";
+            return "недостатня вага";
         }
     }
-
-    public static double CalculateCost(double price, int discount, int count)
+    static double CalculateCost(double price, int discount, int count)
     {
         return price * count * (1 - discount / 100.0);
     }
 
-    public static string GetAgeCategory(int birthYear)
+    static string GetAgeCategory(int birthYear)
     {
-        int currentYear = DateTime.Now.Year;
+        int currentYear = 2026;
         int age = currentYear - birthYear;
         if (age < 18)
         {
-            return "Child";
+            return "Дитина";
         }
         else if (age >= 18 && age < 60)
         {
-            return "Adult";
+            return "Доросла";
         }
         else
         {
-            return "Pensioner";
+            return "Пенсіонер";
         }
     }
-
-    public static string GetPressureStatus(int systolic, int diastolic)
+    static string GetPressureStatus(int systolic, int diastolic)
     {   
         if (systolic < 120 && diastolic < 80)
         {
-            return "Normal";
+            return "Нормальний";
         }else if (systolic < 130 && diastolic < 80)
         {
-        return "Increased";
+        return "Підвищений";
         }else if (systolic < 140 && diastolic < 90)
         {
-        return "1Stage hypertension";
+        return "гіпертензія 1 ступеня";
         }
         else
         {
-        return "2Stage hypertension";
+        return "гіпертензія 2 ступеня";
         } 
     }
     public static void Run()
@@ -74,11 +72,11 @@ public class Task8
         Console.Write("Enter your height ");
         height = int.Parse(Console.ReadLine());
         CalculateBMI(weight, height);
-        Console.WriteLine("BMI: " + GetBMICategory(CalculateBMI(weight, height)));
+        Console.WriteLine("ІМТ: " + GetBMICategory(CalculateBMI(weight, height)));
 
         double bmi = CalculateBMI(weight, height);
         GetBMICategory(bmi);
-        Console.WriteLine("BMI: " + GetBMICategory(bmi));
+        Console.WriteLine("ІМТ: " + GetBMICategory(bmi));
         
         double price = 0;
         int discount, count;
@@ -92,13 +90,12 @@ public class Task8
         Console.WriteLine("Total cost: " + CalculateCost(price, discount, count));
 
         int birthYear = 0;
-        int currentYear = DateTime.Now.Year;
+        int currentYear = 2026;
         Console.Write("Enter your birth year");
         birthYear = int.Parse(Console.ReadLine());
         GetAgeCategory(birthYear);
         int age = currentYear - birthYear;
-        Console.WriteLine("Age category: " + GetAgeCategory(birthYear));
-        Console.WriteLine("Age: " + age);
+        Console.WriteLine($"Вік: {age}, Категорія: {GetAgeCategory(birthYear)}");
         
         int systolic = 0, diastolic = 0;
         Console.Write("Enter your systolic blood pressure");
@@ -106,6 +103,6 @@ public class Task8
         Console.Write("Enter your diastolic blood pressure");
         diastolic = int.Parse(Console.ReadLine());
         GetPressureStatus(systolic, diastolic);
-        Console.WriteLine("Pressure status: " + GetPressureStatus(systolic, diastolic));
+        Console.WriteLine($"Тиск: {systolic} / {diastolic} - {GetPressureStatus(systolic, diastolic)}");
     }
 }
